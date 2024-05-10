9876a38639bf9d6070d05edf8a63616d4d0a7cd1
MuriloPires <133364544+MrlIrving@users.noreply.github.com>
2024-05-10 16:14:47 -0300

    c++

---
+++ Atividade5.cpp
@@ -0,0 +1,94 @@
+////ATIVIDADE1
+//#include <iostream>
+//
+//int main() {
+//    for (int i = 1; i <= 10; ++i) {
+//        std::cout << i << " ";
+//    }
+//    std::cout << std::endl;
+//    return 0;
+//}
+
+////ATIVIDADE2
+//#include <iostream>
+//
+//int main() {
+//    int numero;
+//
+//    std::cout << "Digite um numero: ";
+//    std::cin >> numero;
+//
+//    std::cout << "Tabuada de " << numero << ":" << std::endl;
+//    for (int i = 1; i <= 10; ++i) {
+//        std::cout << numero << " x " << i << " = " << numero * i << std::endl;
+//    }
+//
+//    return 0;
+//}
+
+////ATIVIDADE3
+//#include <iostream>
+//
+//int main() {
+//    for (int i = 0; i <= 50; i += 5) {
+//        std::cout << i << " ";
+//    }
+//    std::cout << std::endl;
+//    return 0;
+//}
+
+////ATIVIDADE4
+//#include <iostream>
+//
+//int main() {
+//    int numero;
+//
+//    for (int i = 0; i < 5; ++i) {
+//        std::cout << "Digite um numero: ";
+//        std::cin >> numero;
+//
+//        if (numero > 100) {
+//            std::cout << "NÚMERO ESPECIAL" << std::endl;
+//        }
+//        else {
+//            std::cout << "NÚMERO COMUM" << std::endl;
+//        }
+//    }
+//
+//    return 0;
+//}
+
+////ATIVIDADE5
+//#include <iostream>
+//
+//int main() {
+//    for (int i = 1; i <= 31; i += 2) {
+//        std::cout << i << " ";
+//    }
+//    std::cout << std::endl;
+//    return 0;
+//}
+
+////ATIVIDADE6
+//#include <iostream>
+//
+//int main() {
+//    int numero;
+//
+//    for (int i = 0; i < 5; ++i) {
+//        std::cout << "Digite um numero: ";
+//        std::cin >> numero;
+//
+//        if (numero > 0) {
+//            std::cout << "NÚMERO POSITIVO" << std::endl;
+//        }
+//        else if (numero < 0) {
+//            std::cout << "NÚMERO NEGATIVO" << std::endl;
+//        }
+//        else {
+//            std::cout << "O NÚMERO É ZERO" << std::endl;
+//        }
+//    }
+//
+//    return 0;
+//}
